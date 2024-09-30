48473dbf62e07c8fa4d85b84e18e06bc6d18d757
Leandro Bellone <Belloneleandro@gmail.com>
2024-09-30 04:26:38 -0300

    Initial commit

---
+++ js/header.js
@@ -0,0 +1,113 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var dropdownInstances = M.Dropdown.init(elems, { hover: false });
+
+    
+    dropdownInstances.forEach(function (dropdownInstance) {
+        if (dropdownInstance) {
+            dropdownInstance.el.addEventListener('click', function () {
+                console.log('Dropdown clickeado');
+                
+                // Ajustar el dropdown cada vez que se haga clic
+                setTimeout(function() {
+                    var dropdown = dropdownInstance.el.nextElementSibling; // Obtiene el dropdown correspondiente
+                    if (dropdown) {
+                        dropdown.style.top = '70px'; // Mover el dropdown a 50px desde la parte superior
+                        console.log('Dropdown abierto, ajustando posición');
+                    } else {
+                        console.error('Dropdown no encontrado');
+                    }
+                }, 0); // Usar un timeout para permitir que se abra antes de ajustar la posición
+            });
+        } else {
+            console.error('Instancia de dropdown no válida');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+});
+
+const sidenavLinks = document.querySelectorAll('.sidenav a'); // Seleccionamos todos los enlaces dentro del sidenav
+const closeDelay = 300; // Retardo en milisegundos
+
+// Función para cerrar el sidenav con retardo
+const closeSidenav = () => {
+    const sidenavInstance = M.Sidenav.getInstance(document.querySelector('.sidenav'));
+    setTimeout(() => {
+        sidenavInstance.close();
+    }, closeDelay); // Se cierra después del retardo
+};
+
+// Agregamos un evento de clic a cada enlace
+sidenavLinks.forEach(link => {
+    link.addEventListener('click', closeSidenav);
+});
+
+// Función para cerrar el sidenav al hacer clic fuera de él
+const closeSidenavOnClickOutside = (event) => {
+    const sidenav = document.querySelector('.sidenav');
+    if (!sidenav.contains(event.target) && sidenav.classList.contains('open')) {
+        closeSidenav();
+    }
+};
+
+// Agregar evento de clic al documento
+document.addEventListener('click', closeSidenavOnClickOutside);
+
+// Variable para almacenar si está haciendo scroll
+let isScrolling = false;
+
+// Función para manejar el scroll
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const ul = document.getElementById('nav-wrapper-ul');
+    const logo = document.getElementById('logo-nav');
+    const titulo = document.getElementById('text1');
+    const titulo2 = document.getElementById('text2');
+    const colorBoxes = document.querySelectorAll('.colorBox'); // Selecciona todos los colorBox
+
+    if (window.scrollY > 0) {
+        // Cuando el usuario ha hecho scroll
+        header.classList.add('header-scrolled');
+        header.classList.remove('header-original');
+        ul.classList.add('ul-scrolled');
+        logo.classList.add('ul-scrolled');
+        logo.classList.add('logo-header-scrolled');
+        titulo.classList.add('text1-scrolled');
+        titulo2.classList.add('text2-scrolled');
+
+        // Cambiar el estilo de hover de colorBox
+        colorBoxes.forEach(box => {
+            box.onmouseover = function () {
+                this.style.background = '#1d1d1d'; // Cambia el color al hacer hover
+            };
+            box.onmouseout = function () {
+                this.style.background = '#1d1d1d'; // Mantiene el color al salir del hover
+            };
+        });
+
+    } else {
+        // Cuando el usuario está en la parte superior de la página
+        header.classList.add('header-original');
+        header.classList.remove('header-scrolled');
+        ul.classList.remove('ul-scrolled');
+        logo.classList.remove('ul-scrolled');
+        logo.classList.remove('logo-header-scrolled');
+        titulo.classList.remove('text1-scrolled');
+        titulo2.classList.remove('text2-scrolled'); 
+
+        // Restaurar el comportamiento original de hover de colorBox
+        colorBoxes.forEach(box => {
+            box.onmouseover = function () {
+                this.style.background = '#00bfff'; // Cambia el color al hacer hover
+            };
+            box.onmouseout = function () {
+                this.style.background = '#1d1d1d'; // Mantiene el color al salir del hover
+            };
+        });
+    }
+};
+
+
