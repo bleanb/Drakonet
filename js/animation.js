963928a65771492e63439aa77a0cd9893895e322
Leandro Bellone <Belloneleandro@gmail.com>
2024-10-01 02:48:05 -0300

    agrego cursos.html

--- js/animation.js
+++
@@ -1,6 +0,0 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => entry.target.classList.toggle("show", entry.isIntersecting))
-})
-
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach(element => observer.observe(element))
